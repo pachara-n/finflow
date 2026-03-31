00ce68ab3e8d68188663b26fa095915e9990ea86
Pachara Nokroy <pacharanokroy2003@gmail.com>
2026-03-31 12:34:55 +0700

    style: set default theme to always dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,8 +42,8 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col font-sans">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="dark"
+          forcedTheme="dark"
           disableTransitionOnChange
         >
           {children}
